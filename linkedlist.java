a4e58cdbf0647b5ed7d06271e45512406d9589f0
Rusell-jay <chingrusell@gmail.com>
2024-10-02 14:55:07 +0800

    linkliststring

---
+++ linkedlist.java
@@ -0,0 +1,46 @@
+package topic_2_linkedlist_string;
+
+public class LinkedList {
+    private Node head;
+
+    // Method to add a new node at the end of the list
+    public void add(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Method to print the linked list
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    // Method to delete a node by value
+    public void deleteByValue(String value) {
+        if (head == null) return;
+        if (head.data.equals(value)) {
+            head = head.next;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data.equals(value)) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+}
